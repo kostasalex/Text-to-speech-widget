2bfba575a971e1484fd6ed8a3551cffe7186be54
Kostas Alex <sdi1700003@di.uoa.gr>
2023-10-06 21:25:12 +0300

    feat(level_1 page): extended the functionality for page level 1
    Extended functionality to allow users to play back audio on detected title tags within the Level 1 page. Enhanced content extraction to handle nested elements better and improved title detection to avoid nav and footer tags.

--- script.js
+++ script.js
@@ -8,15 +8,34 @@ function renderPlayer() {
     addPlayButtonEventListener();
 }
 
+
 function getPotentialTitleElements() {
     const titleTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    const excludedTags = ['nav', 'footer'];
     let titles = [];
-    titleTags.forEach(tag => {
-        const elements = document.querySelectorAll(tag);
-        titles = [...titles, ...elements];
-    });
+    
+    let stack = [document.body];  // Start with the body element
+    
+    while (stack.length > 0) {
+        const current = stack.pop();
+
+        // If it's an excluded element, don't process its children
+        if (!excludedTags.includes(current.tagName.toLowerCase())) {
+            // If it's one of the title tags, add it to the titles array
+            if (titleTags.includes(current.tagName.toLowerCase())) {
+                titles.push(current);
+            }
+            
+            // Add children to the stack to process them
+            stack.push(...Array.from(current.children));
+        }
+    }
+    
     return titles;
 }
+
+
+
 
 function renderPlayerButtons(titles) {
     titles.forEach(title => {
@@ -70,21 +89,46 @@ function addPlayButtonEventListener() {
 
 function speakTitle(button) {
     const title = button.previousSibling.textContent;
+    
     const utterance = new SpeechSynthesisUtterance(title);
     speechSynthesis.speak(utterance);
     return utterance;
 }
 
 function speakContent(button) {
-    const contentElement = button.nextElementSibling; 
-    const contentTexts = Array.from(contentElement.querySelectorAll('p, li, a'))
-                              .map(el => decodeHtmlEntities(el.innerHTML))
+    const contentElement = button.nextElementSibling;
+
+    const contentTexts = getTextNodesIn(contentElement)
+                              .map(node => {
+                                  // Print text before filtering
+                                  console.log("Before decoding:", node.nodeValue);
+
+                                  const decoded = decodeHtmlEntities(node.nodeValue);
+
+                                  // Print text after filtering
+                                  console.log("After decoding:", decoded);
+
+                                  return decoded;
+                              })
                               .join(' ');
-    console.log(contentTexts);
+
     const utterance = new SpeechSynthesisUtterance(contentTexts);
     speechSynthesis.speak(utterance);
     return utterance;
 }
+
+function getTextNodesIn(elem) {
+    let textNodes = [];
+    for (let node of elem.childNodes) {
+        if (node.nodeType === 3) { // text node
+            textNodes.push(node);
+        } else {
+            textNodes = textNodes.concat(getTextNodesIn(node));
+        }
+    }
+    return textNodes;
+}
+
 
 
 function togglePlayStop(button, state) {
@@ -102,7 +146,15 @@ function togglePlayStop(button, state) {
 
 function decodeHtmlEntities(text) {
     const textarea = document.createElement("textarea");
+
+    // Convert line breaks to \n
+    text = text.replace(/<br\s*\/?>/gi, '\n');
+
     textarea.innerHTML = text;
+
     // Strip out HTML tags
-    return textarea.textContent.replace(/<\/?[^>]+(>|$)/g, "");
+    text = textarea.textContent.replace(/<\/?[^>]+(>|$)/g, "");
+
+    // Replace multiple spaces or tabs with a single space
+    return text.replace(/\s+/g, ' ');
 }
